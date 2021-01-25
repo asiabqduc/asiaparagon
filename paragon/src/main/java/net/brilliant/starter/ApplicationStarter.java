7055f6bfc236e6de8d13785e7d6cb267e47a1fcd
ducbq <ducbq@DESKTOP-8NIDEKC>
2021-01-25 07:26:22 +0700

    as

--- paragon/src/main/java/net/brilliant/starter/ApplicationStarter.java
+++ paragon/src/main/java/net/brilliant/starter/ApplicationStarter.java
@@ -16,10 +16,7 @@
 
 package net.brilliant.starter;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 import com.github.adminfaces.starter.config.MasterConfiguration;
@@ -31,7 +28,6 @@ import net.brilliant.config.BaseConfiguration;
  */
 @Import(value = { MasterConfiguration.class, BaseConfiguration.class })
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,WebMvcAutoConfiguration.class })
 public class ApplicationStarter {
 	/*@Inject
 	private Utils utils;
